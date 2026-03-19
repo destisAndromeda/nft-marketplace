bf60fe628ee447ac8179b99c6554ffde2c5eadb2
None <destis_Andromeda@proton.me>
2026-03-19 23:24:57 +0100

    Initial commit

---
+++ programs/nft-marketplace/src/lib.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3xypSWG2NbT5Sx3htRgtqy87AEtyu61tvTp1sJab5o2X");
+
+#[program]
+pub mod nft_marketplace {
+    use super::*;
+
+
+}
---
+++ tests/nft-marketplace.ts
@@ -0,0 +1,7 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { NftMarketplace } from "../target/types/nft_marketplace";
+
+describe("nft-marketplace", () => {
+
+});
