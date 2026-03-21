66b3ac0e17072cee28837e1743df3663ddf617c8
None <destis_Andromeda@proton.me>
2026-03-21 12:48:12 +0100

    feat(instruction): add marketplace_create module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -1,5 +1,7 @@
 pub mod program_config_init;
 pub mod program_config_update;
+pub mod marketplace_create;
 
 pub use program_config_init::*;
 pub use program_config_update::*;
+pub use marketplace_create::*;
