2fd9b825653eb6abe80d4e60fc74832170add8cd
None <destis_Andromeda@proton.me>
2026-03-21 16:41:03 +0100

    feat(instructions): add marketplace_fee_percentage_update module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -1,7 +1,9 @@
 pub mod program_config_init;
 pub mod program_config_update;
 pub mod marketplace_create;
+pub mod marketplace_fee_percentage_update;
 
 pub use program_config_init::*;
 pub use program_config_update::*;
 pub use marketplace_create::*;
+pub use marketplace_fee_percentage_update::*;
