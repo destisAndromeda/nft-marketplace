42ccc81f1925dd6a49f9440384cdc72b453cdb7c
None <destis_Andromeda@proton.me>
2026-03-21 11:24:25 +0100

    feat(marketplace): add Marketplace account struct

---
+++ programs/nft-marketplace/src/state/marketplace.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Marketplace {
+	/// Owners of marketplace
+	pub multisig_owner: Pubkey,
+	/// Account key for lots PDA
+	pub creator_key: Pubkey,
+	/// Marketplace trading fee
+	pub fee_percentage: u64,
+	/// For lot PDA compute
+	pub lot_transaction_index: u64,
+	// Bump for marketplace PDA seed
+	pub bump: u8,
+}
