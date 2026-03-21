35c16fa8dd9a17bf28450ed8984c1f79773453e4
None <destis_Andromeda@proton.me>
2026-03-21 10:38:34 +0100

    feat(program_config_update): add instruction implementation

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -1,3 +1,5 @@
 pub mod program_config_init;
+pub mod program_config_update;
 
 pub use program_config_init::*;
+pub use program_config_update::*;
---
+++ programs/nft-marketplace/src/instructions/program_config_update.rs
@@ -0,0 +1,50 @@
+use anchor_lang::prelude::*;
+
+use crate::state::*;
+use crate::seeds::*;
+use crate::error::*;
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ProgramConfigUpdateArgs {
+    pub authority: Option<Pubkey>,
+
+    pub marketplace_deploy_authority: Option<Pubkey>,
+
+    pub treasury: Option<Pubkey>,
+}
+
+#[derive(Accounts)]
+pub struct ProgramConfigUpdate<'info> {
+    #[account(
+        mut,
+        has_one = authority @ CustomError::Unauthorized,
+        seeds   = [PROGRAM_PREFIX, PROGRAM_CONFIG],
+        bump,
+    )]
+    pub program_config: Account<'info, ProgramConfig>,
+
+    pub authority: Signer<'info>,
+}
+
+impl<'info> ProgramConfigUpdate<'info> {
+    pub fn program_config_update(ctx: Context<Self>, args: ProgramConfigUpdateArgs) -> Result<()> {
+        let program_config = &mut ctx.accounts.program_config;
+
+        match args.authority {
+            None => (),
+            Some(new) => program_config.authority = new,
+        };
+
+        match args.marketplace_deploy_authority {
+            None => (),
+            Some(new) => program_config.marketplace_deploy_authority = new,
+        };
+
+        match args.treasury {
+            None => (),
+            Some(new) => program_config.treasury = new,
+        };
+
+        Ok(())
+    }
+}
