1a355d8aaa91e86263d5d40dccf13b9a23c54c67
None <destis_Andromeda@proton.me>
2026-03-21 18:04:36 +0100

    feat(marketplace_fee_percentage_update): add instruction implementation

--- programs/nft-marketplace/src/instructions/marketplace_fee_percentage_update.rs
+++ programs/nft-marketplace/src/instructions/marketplace_fee_percentage_update.rs
@@ -4,16 +4,45 @@ use crate::state::*;
 use crate::seeds::*;
 use crate::error::*;
 
-// #[derive(AnchorSerialize, AnchorDeserialize)]
-// pub struct MarketplaceFeePercentageUpdateArgs {}
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct MarketplaceFeePercentageUpdateArgs {
+    pub self_index: u64,
 
-// #[derive(Accounts)]
-// pub struct MarketplaceFeePercentageUpdate<'info> {
-//  #[account(
-//      mut,
-//      seeds = [],
-//      bump  = marketplace.bump,
-//  )]
-//  pub marketplace: Account<'info, Marketplace>,
+    pub fee_percentage: u64,
+}
 
-// }
+#[derive(Accounts)]
+#[instruction(args: MarketplaceFeePercentageUpdateArgs)]
+pub struct MarketplaceFeePercentageUpdate<'info> {
+    #[account(
+        mut,
+        has_one = multisig_owner @ CustomError::Unauthorized,
+        seeds   = [
+            PROGRAM_PREFIX,
+            multisig_owner.key().as_ref(),
+            MARKETPLACE,
+            &args.self_index.to_le_bytes(),
+        ],
+        bump  = marketplace.bump,
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+
+    pub multisig_owner: Signer<'info>,
+
+    #[account(
+        seeds = [PROGRAM_PREFIX, PROGRAM_CONFIG],
+        bump  = program_config.bump,
+    )]
+    pub program_config: Account<'info, ProgramConfig>,
+}
+
+impl<'info> MarketplaceFeePercentageUpdate<'info> {
+    pub fn marketplace_fee_percentage_update(ctx: Context<Self>, args: MarketplaceFeePercentageUpdateArgs) -> Result<()> {
+        let marketplace = &mut ctx.accounts.marketplace;
+        if args.fee_percentage > 0 {
+            marketplace.fee_percentage = args.fee_percentage;
+        }
+
+        Ok(())
+    }
+}
