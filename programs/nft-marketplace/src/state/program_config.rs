444562b5207d208a0334018ec18724df74bec1ef
None <destis_Andromeda@proton.me>
2026-03-21 10:45:35 +0100

    refactor(program_config): delete ProgramConfigOld struct

--- programs/nft-marketplace/src/state/program_config.rs
+++ programs/nft-marketplace/src/state/program_config.rs
@@ -2,17 +2,6 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct ProgramConfigOld {
-    /// Authority that can change PorgramConfig state
-    pub authority: Pubkey,
-    /// For Marketplace deploying
-    pub marketplace_creation_fee: u64,
-    /// Treasury of Marketplace fee
-    pub treasury: Pubkey,
-}
-
-#[account]
-#[derive(InitSpace)]
 pub struct ProgramConfig {
     /// Authority that can change ProgramConfig state
     pub authority: Pubkey,
