f682a887ad5bbaf638950ef7555e54400c05b2c6
None <destis_Andromeda@proton.me>
2026-03-22 09:50:17 +0100

    feat(instructions): add place_lot module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -3,9 +3,11 @@ pub mod program_config_update;
 pub mod marketplace_create;
 pub mod marketplace_fee_percentage_update;
 pub mod lot_create;
+pub mod place_lot;
 
 pub use program_config_init::*;
 pub use program_config_update::*;
 pub use marketplace_create::*;
 pub use marketplace_fee_percentage_update::*;
 pub use lot_create::*;
+pub use place_lot::*;
