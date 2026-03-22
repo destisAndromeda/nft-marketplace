bd12bc9f7dc169f10ce5da1ce4a78eeeb67b4b3a
None <destis_Andromeda@proton.me>
2026-03-22 18:05:17 +0100

    feat(instructions): add buy_nft module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -4,9 +4,7 @@ pub mod marketplace_create;
 pub mod marketplace_fee_percentage_update;
 pub mod lot_create;
 pub mod change_lot_status;
-
-// pub mod place_lot;
-// pub mod make_lot_available_for_sale;
+pub mod buy_nft;
 
 pub use program_config_init::*;
 pub use program_config_update::*;
@@ -14,6 +12,4 @@ pub use marketplace_create::*;
 pub use marketplace_fee_percentage_update::*;
 pub use lot_create::*;
 pub use change_lot_status::*;
-
-// pub use place_lot::*;
-// pub use make_lot_available_for_sale::*;
+pub use buy_nft::*;
