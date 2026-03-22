1150cd0fed84ec66a10dc8364bad2dd8caa66d24
None <destis_Andromeda@proton.me>
2026-03-22 11:43:25 +0100

    feat(instructions): add make_lot_available_for_sale module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -4,6 +4,7 @@ pub mod marketplace_create;
 pub mod marketplace_fee_percentage_update;
 pub mod lot_create;
 pub mod place_lot;
+pub mod make_lot_available_for_sale;
 
 pub use program_config_init::*;
 pub use program_config_update::*;
@@ -11,3 +12,4 @@ pub use marketplace_create::*;
 pub use marketplace_fee_percentage_update::*;
 pub use lot_create::*;
 pub use place_lot::*;
+pub use make_lot_available_for_sale::*;
