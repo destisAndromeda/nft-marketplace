7e5c5e56015e85accf537172b64818be8e21f181
None <destis_Andromeda@proton.me>
2026-03-22 12:47:07 +0100

    feat(instructions): add change_lot_status module

--- programs/nft-marketplace/src/instructions.rs
+++ programs/nft-marketplace/src/instructions.rs
@@ -3,8 +3,12 @@ pub mod program_config_update;
 pub mod marketplace_create;
 pub mod marketplace_fee_percentage_update;
 pub mod lot_create;
+pub mod change_lot_status;
+
 pub mod place_lot;
 pub mod make_lot_available_for_sale;
+
+
 
 pub use program_config_init::*;
 pub use program_config_update::*;
@@ -13,3 +17,4 @@ pub use marketplace_fee_percentage_update::*;
 pub use lot_create::*;
 pub use place_lot::*;
 pub use make_lot_available_for_sale::*;
+pub use change_lot_status::*;
